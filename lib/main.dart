5011514b25b72f3b2d06b9fb782d9401bf76bf76
Muhammed Tash <119746836+M26Tash@users.noreply.github.com>
2025-10-26 11:11:44 +0300

    Merge 'feature/supabase-integration' to 'dev' (#3)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:t_rent/src/common/di/injector.dart';
 import 'package:t_rent/src/features/app/page/app.dart';
 
@@ -11,7 +12,15 @@ void main() async {
 
   await dotenv.load();
 
-  final accessToken = dotenv.get('ACCESSTOKEN');
+  final supabaseUrl = dotenv.get('SUPABASE_URL');
+  final supabaseAnonKey = dotenv.get('SUPABASE_ANON_KEY');
+  final accessToken = dotenv.get('ACCESS_TOKEN');
+
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseAnonKey,
+    debug: true,
+  );
 
   MapboxOptions.setAccessToken(accessToken);
 
